f3231cc5fff771d5c2d2662fcc7feed7cd7ec995
snamiki1212 <snamiki1212@gmail.com>
2021-07-07 14:15:04 -0700

    Add parser

--- math/main.m
+++ math/main.m
@@ -7,10 +7,32 @@
 
 #import <Foundation/Foundation.h>
 
+// MARK: - Input Helpers
+NSString *getInputStr(void) {
+    char inputChars[255];
+    printf("Input a string: ");
+    fgets(inputChars, 255, stdin);
+//    NSString *inputString = [NSString stringWithUTF8String:inputChars];
+    NSString *inputString = [NSString stringWithCString:inputChars encoding:(NSUTF8StringEncoding)];
+
+    return inputString;
+}
+
+NSString *parseStr(NSString *str) {
+    return [str stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+}
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
         NSLog(@"Hello, World!");
+        
+        NSString *str = getInputStr();
+        str = parseStr(str);
+        NSLog(@"%@.", str);
+        if([str isEqualToString:@"text"]) {
+            printf("OK\n");
+        }
     }
     return 0;
 }
